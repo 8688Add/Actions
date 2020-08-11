daa54e93879f7291e4c45beaccc58da41de1af64
8688Add <42204083+8688Add@users.noreply.github.com>
2020-08-11 23:49:00 +0800

    Update leantest.sh

--- leantest.sh
+++ leantest.sh
@@ -11,12 +11,12 @@ sed -i 's/KERNEL_PATCHVER:=5.4/KERNEL_PATCHVER:=4.14/g' target/linux/ramips/Make
 sed -i 's/KERNEL_TESTING_PATCHVER:=5.4/KERNEL_TESTING_PATCHVER:=4.14/g' target/linux/ramips/Makefile
 #git clone https://github.com/project-openwrt/luci-app-koolproxyR.git package/luci-app-koolproxyR
 #git clone https://github.com/fw876/helloworld.git package/luci-app-ssr-plus
-git clone https://github.com/0118Add/luci-app-ssr-plus.git package/luci-app-ssr-plus
+#git clone https://github.com/0118Add/luci-app-ssr-plus.git package/luci-app-ssr-plus
 #git clone https://github.com/8688Add/kenzok8-openwrt.git package/kenzok8-openwrt
 #git clone https://github.com/tty228/luci-app-serverchan.git package/luci-app-serverchan
 #git clone https://github.com/Lienol/openwrt-package.git package/luci-app-passwall
 #git clone https://github.com/Lienol/openwrt-package.git package/luci-theme-argon-dark-mod
-#git clone https://github.com/8688Add/OpenClash.git package/luci-app-openclash
+git clone https://github.com/8688Add/OpenClash.git package/luci-app-openclash
 #git clone https://github.com/destan19/OpenAppFilter.git package/luci-app-oaf
 #git clone https://github.com/jerrykuku/luci-app-vssr.git package/luci-app-vssr
 #git clone https://github.com/jerrykuku/lua-maxminddb.git package/lua-maxminddb
