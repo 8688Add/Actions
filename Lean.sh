73e97565e7ef7cb20b0d490159a9cd520195e732
8688Add <42204083+8688Add@users.noreply.github.com>
2020-08-12 20:52:33 +0800

    Update Lean.sh

--- Lean.sh
+++ Lean.sh
@@ -11,16 +11,19 @@
 #sed -i '/uci commit luci/i\uci set luci.main.mediaurlbase=/luci-static/argon' package/lean/default-settings/files/zzz-default-settings
 #修改版本号
 #sed -i 's/OpenWrt/Bin AutoBuild $(TZ=UTC-8 date "+%Y.%m.%d") @ OpenWrt/g' package/lean/default-settings/files/zzz-default-settings
-git clone https://github.com/Lienol/openwrt-package.git package/luci-app-ssr-mudb-server
-git clone https://github.com/0118Add/luci-app-ssr-plus.git package/luci-app-ssr-plus
-git clone https://github.com/Lienol/openwrt-package.git package/luci-theme-argon-dark-mod
-git clone https://github.com/8688Add/luci-app-passwall.git package/luci-app-passwall
-git clone https://github.com/8688Add/small.git package/small
-git clone https://github.com/8688Add/luci-app-koolproxyR.git package/luci-app-koolproxyR
-git clone https://github.com/8688Add/OpenClash.git package/luci-app-openclash
+#git clone https://github.com/tty228/luci-app-serverchan.git package/luci-app-serverchan
+git clone https://github.com/jerrykuku/luci-app-vssr package/luci-app-vssr
+git clone https://github.com/jerrykuku/lua-maxminddb.git package/lua-maxminddb
+#git clone https://github.com/0118Add/luci-app-ssr-plus.git package/luci-app-ssr-plus
+git clone https://github.com/Lienol/openwrt-package.git package/openwrt-package
+#git clone https://github.com/8688Add/luci-app-koolproxyR.git package/luci-app-koolproxyR
+git clone -b master https://github.com/vernesong/OpenClash.git package/luci-app-openclash
+#git clone https://github.com/frainzy1477/luci-app-clash.git package/luci-app-clash
 git clone https://github.com/8688Add/luci-theme-infinityfreedom.git package/luci-theme-infinityfreedom
 git clone https://github.com/8688Add/luci-theme-opentomato.git package/luci-theme-opentomato
 rm -rf package/lean/luci-theme-argon
 git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git package/lean/luci-theme-argon
+git clone -b 18.06 https://github.com/garypang13/luci-theme-edge package/luci-theme-edge
 git clone https://github.com/jerrykuku/luci-app-jd-dailybonus.git package/luci-app-jd-dailybonus
 git clone https://github.com/jerrykuku/node-request.git package/node-request
+git clone https://github.com/riverscn/openwrt-iptvhelper.git package/luci-app-iptvhelper
