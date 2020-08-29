19088cf29bae6241f620881e3ab86840d70636b9
8688Add <42204083+8688Add@users.noreply.github.com>
2020-08-30 01:01:11 +0800

    Update test.sh

--- test.sh
+++ test.sh
@@ -12,4 +12,4 @@
 #修改版本号
 #sed -i 's/OpenWrt/Bin AutoBuild $(TZ=UTC-8 date "+%Y.%m.%d") @ OpenWrt/g' package/lean/default-settings/files/zzz-default-settings
 #git clone https://github.com/project-openwrt/luci-app-koolproxyR.git package/luci-app-koolproxyR
-#git clone https://github.com/8688Add/bf-package.git package/bf-package
+git clone https://github.com/8688Add/bf-package-master.git package/bf-package-master
