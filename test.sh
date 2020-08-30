8afe9ff1b5046f6a56c48859280a598941da9e73
8688Add <42204083+8688Add@users.noreply.github.com>
2020-08-30 22:14:08 +0800

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,5 +1,5 @@
-#rm -rf package/ctcgfw/luci-app-vssr
-#git clone https://github.com/ChrisLiu0343/luci-app-vssr-2.git package/ctcgfw/luci-app-vssr
+rm -rf package/ctcgfw/luci-app-vssr
+git clone https://github.com/jerrykuku/luci-app-vssr.git package/ctcgfw/luci-app-vssr
 #git clone https://github.com/Leo-Jo-My/diy.git package/diy
 #rm -rf package/lean/luci-app-ssr-plus
 #git clone https://github.com/8688Add/luci-app-ssr-plus.git package/lean/luci-app-ssr-plus
@@ -12,4 +12,4 @@
 #修改版本号
 #sed -i 's/OpenWrt/Bin AutoBuild $(TZ=UTC-8 date "+%Y.%m.%d") @ OpenWrt/g' package/lean/default-settings/files/zzz-default-settings
 #git clone https://github.com/project-openwrt/luci-app-koolproxyR.git package/luci-app-koolproxyR
-git clone https://github.com/8688Add/luci-app-flowoffload.git package/luci-app-flowoffload
+git clone https://github.com/8688Add/openwrt-bf.git package/openwrt-bf
