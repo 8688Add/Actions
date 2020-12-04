c5afe43907296d7ac82643bdb7b7977cdc417ea1
8688Add <42204083+8688Add@users.noreply.github.com>
2020-12-05 06:28:38 +0800

    Update leantest.sh

--- leantest.sh
+++ leantest.sh
@@ -26,6 +26,16 @@ chmod 0755 package/lean/luci-app-frpc/root/etc/init.d/frp
 rm -rf package/lean/luci-app-unblockmusic
 svn co https://github.com/project-openwrt/openwrt/trunk/package/lean/luci-app-unblockmusic package/lean/luci-app-unblockmusic
 
+svn co https://github.com/fw876/helloworld/trunk/tcping package/lean/tcping
+svn co https://github.com/coolsnowwolf/lede/trunk/package/lean/shadowsocksr-libev package/lean/shadowsocksr-libev
+svn co https://github.com/coolsnowwolf/lede/trunk/package/lean/pdnsd-alt package/lean/pdnsd-alt
+svn co https://github.com/coolsnowwolf/lede/trunk/package/lean/microsocks package/lean/microsocks
+svn co https://github.com/coolsnowwolf/lede/trunk/package/lean/dns2socks package/lean/dns2socks
+svn co https://github.com/coolsnowwolf/lede/trunk/package/lean/simple-obfs package/lean/simple-obfs
+svn co https://github.com/coolsnowwolf/lede/trunk/package/lean/trojan package/lean/trojan
+svn co https://github.com/coolsnowwolf/lede/trunk/package/lean/ipt2socks package/lean/ipt2socks
+svn co https://github.com/coolsnowwolf/lede/trunk/package/lean/redsocks2 package/lean/redsocks2
+
 sed -i 's/"Turbo ACC 网络加速"/"网络加速"/g' package/lean/luci-app-flowoffload/po/zh-cn/flowoffload.po
 sed -i 's/"Turbo ACC 网络加速"/"网络加速"/g' package/lean/luci-app-sfe/po/zh-cn/sfe.po
 #sed -i 's/"ShadowSocksR Plus+"/"SSR Plus+"/g' package/luci-app-ssr-plus/luasrc/controller/shadowsocksr.lua
